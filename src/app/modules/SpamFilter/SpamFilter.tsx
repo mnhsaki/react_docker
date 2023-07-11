cb231b13238185286667ed411a0e38932130d244
Hafiz51 <94961088+Hafiz51@users.noreply.github.com>
2023-07-11 16:32:38 +0600

    modify the spam filter table

--- src/app/modules/SpamFilter/SpamFilter.tsx
+++ src/app/modules/SpamFilter/SpamFilter.tsx
@@ -1,23 +1,69 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Table } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import AddNewFilterModal from './AddNewFilterModal';
 
-const SpamFilter = () => {
+interface DataEntry {
+  id: number;
+  name: string;
+  age: number;
+}
 
-  const data = [
+const SpamFilter = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+  const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc');
+  const [sortBy, setSortBy] = useState<keyof DataEntry>('id');
+  
+  const data: DataEntry[] = [
     { id: 1, name: 'John Doe', age: 25 },
     { id: 2, name: 'Jane Smith', age: 30 },
     // Add more data entries here...
   ];
 
+  const totalPages = Math.ceil(data.length / pageSize);
+  const indexOfLastItem = currentPage * pageSize;
+  const indexOfFirstItem = indexOfLastItem - pageSize;
+  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
 
   useEffect(() => {
     document.title = 'SpamFilter';
   }, []);
 
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const handlePageSizeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setPageSize(parseInt(event.target.value));
+    setCurrentPage(1); // Reset to the first page when changing page size
+  };
+
+  const handleSortChange = (column: keyof DataEntry) => {
+    if (sortBy === column) {
+      setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+    } else {
+      setSortBy(column);
+      setSortOrder('asc');
+    }
+  };
+
+  const sortedData = [...data].sort((a, b) => {
+    if (sortOrder === 'asc') {
+      if (a[sortBy] < b[sortBy]) return -1;
+      if (a[sortBy] > b[sortBy]) return 1;
+      return 0;
+    } else {
+      if (a[sortBy] < b[sortBy]) return 1;
+      if (a[sortBy] > b[sortBy]) return -1;
+      return 0;
+    }
+  });
+
   return (
     <div>
+      
+
       <div className='card mb-5 mb-xl-10'>
         <div
           className='card-header border-0 cursor-pointer'
@@ -52,28 +98,69 @@ const SpamFilter = () => {
           </div>
         </div>
       </div>
-
+      <div className="page-size-options">
+        <span>Show:</span>
+        <select value={pageSize} onChange={handlePageSizeChange}>
+          <option value={10}>10</option>
+          <option value={50}>50</option>
+          <option value={100}>100</option>
+          <option value={500}>500</option>
+        </select>
+      </div>
       <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Name</th>
-          <th>Age</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((entry) => (
-          <tr key={entry.id}>
-            <td>{entry.id}</td>
-            <td>{entry.name}</td>
-            <td>{entry.age}</td>
+        <thead>
+          <tr>
+            <th>
+              <Button
+                variant="link"
+                onClick={() => handleSortChange('id')}
+              >
+                ID {sortBy === 'id' && <span>&#x25BE;</span>}
+              </Button>
+            </th>
+            <th>
+              <Button
+                variant="link"
+                onClick={() => handleSortChange('name')}
+              >
+                Name {sortBy === 'name' && <span>&#x25BE;</span>}
+              </Button>
+            </th>
+            <th>
+              <Button
+                variant="link"
+                onClick={() => handleSortChange('age')}
+              >
+                Age {sortBy === 'age' && <span>&#x25BE;</span>}
+              </Button>
+            </th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {sortedData.map((entry) => (
+            <tr key={entry.id}>
+              <td>{entry.id}</td>
+              <td>{entry.name}</td>
+              <td>{entry.age}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
 
+      <div className="pagination">
+        {Array.from({ length: totalPages }, (_, index) => (
+          <Button
+            key={index + 1}
+            variant={currentPage === index + 1 ? 'primary' : 'light'}
+            size='sm'
+            onClick={() => handlePageChange(index + 1)}
+          >
+            {index + 1}
+          </Button>
+        ))}
+      </div>
     </div>
   );
 };
 
-export default SpamFilter
+export default SpamFilter;
